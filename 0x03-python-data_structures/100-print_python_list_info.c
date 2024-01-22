228b346bbad278ee84aca6f49c54a1c95547178d
Wanjjiru <purewanjjiru17@gmail.com>
2024-01-22 19:54:33 +0000

    Palindrome

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,12 +4,12 @@
 
 void print_python_list_info(PyObject *p)
 {
-	long int measure = PyList_size(p);
+	long int measure = PyList_Size(p);
 	int x;
 	PyListObject *objct = (PyListObject *)p;
 
 	printf("[*] Size of the Python List = %li\n", measure);
 	printf("[*] Allocated = %li\n", objct->allocated);
 	for (x = 0; x < measure; x++)
-		printf("Element %x: %s\n", x, Py_TYPE(objct->ob_item[x])->tp_name);
+		printf("Element %x: %s\n", x, PY_TYPE(objct->ob_item[x])->tp_name);
 }
