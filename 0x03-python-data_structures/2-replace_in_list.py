3783792880236a7b799d16578252bc29970c9ca6
Wanjjiru <purewanjjiru17@gmail.com>
2024-01-22 19:26:37 +0000

    o2-file

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    temporary = my_list[:]
     if idx < 0:
-        return (my_list)
+        return (temporary)
     if idx > len(my_list) - 1:
-        return(my_list)
-    my_list[idx] = element
-    return (my_list)
+        return(temporary)
+    temporary[idx] = element
+    return (temporary)
